94824c7ceeff68bb3da5527272590bc3409be441
MIARD <minhajul.abedin.rd@gmail.com>
2021-01-07 21:19:32 +0600

    Add Messages

--- App_Login/views.py
+++ App_Login/views.py
@@ -8,8 +8,11 @@ from django.contrib.auth import login, logout, authenticate
 
 # Forms and Models
 from App_Login.models import Profile
-from App_Login.forms ProfileForm, SignUpForm
-# Create your views here.
+from App_Login.forms import ProfileForm, SignUpForm
+
+# Messages
+from django.contrib import messages
+
 
 def sign_up(request):
     form = SignUpForm()
@@ -17,15 +20,16 @@ def sign_up(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Account Created Successfully!")
             return HttpResponseRedirect(reverse('App_Login:login'))
-    return render(request, 'App_Login/sign_up.html', context={'form':form})
+    return render(request, 'App_Login/signup.html', context={'form':form})
 
 def login_user(request):
     form = AuthenticationForm()
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('email')
+            username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username,password=password)
             if user is not None:
@@ -36,4 +40,18 @@ def login_user(request):
 @login_required
 def logout_user(request):
     logout(request)
+    messages.warning(request, "You Are Logged Out!")
     return HttpResponseRedirect(reverse('App_Login:login'))
+
+@login_required
+def user_profile(request):
+    profile = Profile.objects.get(user=request.user)
+
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Updated Successfully!')
+            form = ProfileForm(instance=profile)
+    return render(request, 'App_Login/change_profile.html', context={'form':form})
